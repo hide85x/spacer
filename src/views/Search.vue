70803585ffc7ab00e562292b68939ae387287ea3
hide <hide85x@gmail.com>
2020-05-26 21:39:45 +0200

    bg image and logo

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,67 +3,54 @@
     <div class="search">
       <label for="search">Search</label>
       <input v-model="search_query" id="search" type="text" name="search" @keypress="inputHandler" />
-      <div>
-        <h1>dd</h1>
-      </div>
+      <ul>
+        <li v-for="result in results" :key="result.data[0].nasa_id">
+          {{result.data[0]}}
+        </li>
+      </ul>
+      <!-- <div id="img">
+        <h1>image</h1>
+        <img id="image" alt />
+      </div>-->
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import debounce from 'lodash.debounce' ;
 const url_base = "https://images-api.nasa.gov/search?q=";
+
 export default {
   name: "Search",
   data() {
     return {
-      search_query: "" // daje nam dostep to two way binding w  vmodel w input, musi miec inicjalną wartość!
+      search_query: "" , // daje nam dostep to two way binding w  vmodel w input, musi miec inicjalną wartość!
+      results: []
     };
   },
   methods: {
-    inputHandler(e) {
-      if (e.key == "Enter") {
-        console.log(this.search_query);
+    inputHandler: debounce(function() {
+      const imageTag = document.querySelector("#image");
+
         axios
           .get(`${url_base}${this.search_query}&media_type=image`)
           .then(res => {
-            console.log(res);
+            !res.data.collection.items.length ? alert('couldnt find anything....'): null;
+              
+            
+            this.results = res.data.collection.items;
+
           })
           .catch(err => {
             console.log(err);
           });
-      } else {
-        return;
-      }
-    }
-  }
+      }, 500 )   
+}
+  
 };
 </script>
 
 <style lang="scss">
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  padding: 30px;
-  margin: 0;
-}
-.search {
-  width: 300px;
-  display: flex;
-  flex-direction: column;
-
-  label {
-    font-family: "Montserrat", sans-serif;
-    align-self: center;
-  }
-
-  input {
-    height: 50px;
-    border: none;
-    border-bottom: 1px solid black;
-    text-align: center;
-  }
-}
+@import "./style.scss";
 </style>
